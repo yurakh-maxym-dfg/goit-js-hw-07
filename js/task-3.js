521415637b97b46f92b584f63a52d5ef101468d1
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-04-05 21:27:02 +0300

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', (event) => {
+    const blank = output.textContent = event.currentTarget.value.trim();
+    output.textContent = blank === "" ? "Anonymous" : blank;
+});
