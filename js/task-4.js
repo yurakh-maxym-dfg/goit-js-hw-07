42b9dec26dcdaef7be6c52e3402b76cb6a9168de
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-04-06 14:58:12 +0300

    task-4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {email, password} = event.currentTarget.elements;
+
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    }
+
+    if (emailValue === '' || passwordValue === ''){
+        alert('All form fields must be filled in');
+        return;
+    }
+    
+
+    console.log(formData);
+    event.currentTarget.reset();
+}
