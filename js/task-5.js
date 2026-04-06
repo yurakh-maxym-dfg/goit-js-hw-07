4fb870a55a87650e6497574720ca5c6af5c5b401
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-04-06 15:20:11 +0300

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,17 @@
 'use strict';
 
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorBtn = document.querySelector('.change-color');
+const colorTxt = document.querySelector('.color');
+
+colorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+
+  colorTxt.textContent = color;
+  document.body.style.backgroundColor = color;
+});
